1ca0bf3c5f94d81bc6d72f7b6c6a49efb398b8bd
Mohammed <jahinarham2@icloud.com>
2022-04-15 21:05:54 +0400

    first commit
    adding the source files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "livechat52d51903": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "livechat": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Amplify from "aws-amplify";
+import awsconfig from "../aws-exports";
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+Amplify.configure({ ...awsconfig, ssr: true });
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+export default withAuthenticator(App); //using AWS AMplify default signup form css
 
-export default MyApp
+
+
+
+
+
+
